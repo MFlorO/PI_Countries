bbe1da95ca3e103c0904904c0da88d267c1f0f0e
Flori O <floroldani@hotmail.com>
2022-07-25 13:32:20 -0300

    Landing

--- client/src/componentes/Landing/Landing.jsx
+++ client/src/componentes/Landing/Landing.jsx
@@ -1,18 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./landing.css";
+import imagen1 from "./imagenes/1.jpg"
+import imagen2 from "./imagenes/2.jpg"
+import imagen3 from "./imagenes/3.jpg"
+import imagen4 from "./imagenes/4.jpg"
+
+
+
 
 
 
 export default function Landing() {
   return (
+    <>
     <div className="slider">
+      <ul className="ul">
+        <li className="li">
+          <img src={imagen1} alt="" />
+        </li>
+        <li className="li">
+          <img src={imagen2} alt="" />
+        </li>
+        <li className="li">
+          <img src={imagen3} alt="" />
+        </li>
+        <li className="li">
+          <img src={imagen4} alt="" />
+        </li>
+        {/* <li className="li">
+          <img src={imagen5} alt="" />
+        </li> */}
+      </ul>
+    </div>
 
-        <h1>Welcome to Countries</h1>
+        <h1 className="title">Welcome to Countries</h1>
 
         <Link to="/home">
           <button className="boton">INGRESAR</button>
         </Link> 
 
-    </div>
+    
+
+    </>
   );
 }
