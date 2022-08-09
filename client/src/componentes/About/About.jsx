48954e7cbbdc2beb1a190f33d581ce0467128ae4
Flori O <floroldani@hotmail.com>
2022-08-09 00:14:20 -0300

    Front

--- client/src/componentes/About/About.jsx
+++ client/src/componentes/About/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Footer from "./Footer/Footer.jsx"
 import "./About.css"
-import { SiRedux, SiPostgresql } from "react-icons/si"
+import { SiRedux, SiPostgresql, SiSequelize } from "react-icons/si"
 import { FaReact, FaNode } from "react-icons/fa"
 import { IoLogoJavascript } from "react-icons/io"
 
@@ -18,15 +18,17 @@ function About() {
 
             <p className="subtitulo-2">This project was done through the use of an external API that obtains information about countries: {<a href="https://restcountries.com/">Restcountries</a>}</p>
             <p className="subtitulo-2">Then through a controlled form I create tourist activities and relate them to these countries</p>
-
+            <p className="subtitulo-2">Also, I can create a list of my favorite countries with their respective tourist activities</p>
+            
             <div className="logos">
                 <h3 className="logo-titulo">The skills used in this project are:</h3>
                 <div className="logos-items">
-                <p className="icono"><SiRedux className="icono-1" size="2rem"/></p>
-                <FaReact size="2rem"/>
-                <IoLogoJavascript size="2rem"/>
-                <SiPostgresql size="2rem"/>
-                <FaNode size="2rem"/>
+                   <SiRedux className="icono-1" size="2rem"/>
+                   <FaReact size="2rem"/>
+                   <IoLogoJavascript size="2rem"/>
+                   <SiPostgresql size="2rem"/>
+                   <SiSequelize size="2rem"/>
+                   <FaNode size="2rem"/>
                 </div>
             </div>
 
