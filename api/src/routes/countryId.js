3a165cc505119ac1679f333837b95790b22edffc
Flori O <floroldani@hotmail.com>
2022-08-17 00:24:34 -0300

    cambiosApi

--- api/src/routes/countryId.js
+++ api/src/routes/countryId.js
@@ -39,5 +39,59 @@ router.get("/:id", async function (req, res) {
 
 
 
+
+
+
+
+
+
 module.exports = router;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get("/:id", async function (req, res) {
+
+    const { id } = req.params;
+
+
+    
+        const countryBD = await Country.findOne({
+            where: { id:id.toUpperCase() }, //El toUpperCase() Lo uso por si escribo "CFK" o "cfk"
+            include: {
+                model: Activity, 
+                attributes: ['id',"name","difficulty", "duration", "seassion"],
+                through: { attributes: [] }
+            }
+        })
+
+       
+
+    .then(countryBD => {
+        res.json(countryBD)
+    })
+
+
+    .catch (error => {
+        res.status(404).send("Not recived id")
+    })
+})
